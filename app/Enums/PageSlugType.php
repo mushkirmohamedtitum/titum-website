acf137881f7df89dc06d9537633df16ece30e647
Mohamed Mushkir <mushkir@titumlk.onmicrosoft.com>
2025-08-25 17:36:05 +0530

    feat: implemented guest-navigation with ssr

---
+++ app/Enums/PageSlugType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum PageSlugType: string
+{
+    case SERVICES = "services";
+    case CLIENTS = "clients";
+    case ABOUT_US = "about-us";
+    case CONTACT_US = "contact-us";
+}
--- app/Http/Controllers/Guest/ServiceController.php
+++ app/Http/Controllers/Guest/ServiceController.php
@@ -2,13 +2,42 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Enums\PageSlugType;
 use App\Http\Controllers\Controller;
+use App\Models\Page;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+use function Pest\Laravel\json;
 
 class ServiceController extends Controller
 {
     public function show($slug)
     {
-        return $slug;
+        $pageSlug = PageSlugType::tryFrom($slug);
+
+        $page = Page::where('slug', $pageSlug)->first();
+
+        $data = ['page' => $page];
+
+        if ($pageSlug === PageSlugType::SERVICES) {
+
+            return Inertia::render('TheServicesPage', $data);
+        }
+
+        if ($pageSlug === PageSlugType::ABOUT_US) {
+
+            return Inertia::render('TheAboutUsPage', $data);
+        }
+
+        if ($pageSlug === PageSlugType::CLIENTS) {
+
+            return Inertia::render('TheClientsPage', $data);
+        }
+
+        if ($pageSlug === PageSlugType::CONTACT_US) {
+
+            return Inertia::render('TheContactUsPage', $data);
+        }
     }
 }
