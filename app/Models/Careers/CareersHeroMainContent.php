2d260796b27a6b2d4972e9f8d1e6249d89cbf3aa
Mohamed Mushkir <mushkir@titumlk.onmicrosoft.com>
2025-12-08 10:10:49 +0530

    init: initialized career-hero-main-content model relationships

--- app/Models/Careers/CareersHeroMainContent.php
+++ app/Models/Careers/CareersHeroMainContent.php
@@ -2,6 +2,9 @@
 
 namespace App\Models\Careers;
 
+use App\Enums\ModelColumns\CommonColumns;
+use App\Enums\StateTypes;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,4 +14,19 @@ class CareersHeroMainContent extends Model
     use HasFactory, HasUuids;
 
     protected $guarded = [];
+
+    public function scopeActive($query)
+    {
+        return $query->where(CommonColumns::IS_ACTIVE, StateTypes::ACTIVE);
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, CommonColumns::CREATED_BY, CommonColumns::ID);
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, CommonColumns::UPDATED_BY, CommonColumns::ID);
+    }
 }
